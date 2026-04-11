15c06aa22cc9f395ab7f154ca7da8f249b314a06
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2026-04-11 21:16:21 +0200

    fix: update WhatsApp number in constants

--- chance-web/src/lib/constants.ts
+++ chance-web/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "250780000000";
+export const WHATSAPP_NUMBER = "250786069425";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
 export const whatsappMsg = (text: string) =>
   `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(text)}`;
